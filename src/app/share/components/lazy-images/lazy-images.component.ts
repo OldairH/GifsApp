58db183a11b7b57b59cd174ddad4519f440314f5
Oldair Huillca <olhuillcahumpire@gmail.com>
2024-10-22 15:55:14 -0500

    ultimo avance

--- src/app/share/components/lazy-images/lazy-images.component.ts
+++ src/app/share/components/lazy-images/lazy-images.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'share-lazy-images',
   templateUrl: './lazy-images.component.html',
   styleUrl: './lazy-images.component.css'
 })
-export class LazyImagesComponent {
+export class LazyImagesComponent implements OnInit {
 
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  private hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL property is required.');
+  }
 }
